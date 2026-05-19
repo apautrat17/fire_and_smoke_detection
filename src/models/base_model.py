3a66e6e67efe494e3cc831013154fd994690ade7
apautrat <anthony.pautrat@delfox.net>
2026-05-19 15:58:08 +0200

    Improve and optimize model architecture

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -3,29 +3,26 @@ from ultralytics import YOLO
 from src.main import config
 
 
+class Head(nn.Module):
+    def __init__(self, c, num_classes):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(c, c, 3, padding=1), nn.SiLU(), nn.Conv2d(c, 5 + num_classes, 1)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
 class FireSmokeModel(nn.Module):
     def __init__(self, backbone, in_channels, num_classes=2):
         super().__init__()
         self.backbone = backbone
-        self.head = nn.Conv2d(
-            in_channels=in_channels, out_channels=5 + num_classes, kernel_size=1
-        )
+        self.head = Head(in_channels, num_classes)
 
     def forward(self, x):
         x = self.backbone(x)
         return self.head(x)
-
-
-class YOLOFeatureBackbone(nn.Module):
-    def __init__(self, yolo_model, hook_key="x", feature_maps={}):
-        super().__init__()
-        self.yolo_model = yolo_model
-        self.hook_key = hook_key
-        self.feature_maps = feature_maps
-
-    def forward(self, x):
-        _ = self.yolo_model(x)
-        return self.feature_maps[self.hook_key]
 
 
 def create_fire_smoke_model():
@@ -34,15 +31,8 @@ def create_fire_smoke_model():
 
     yolo_nn = yolo_model.model
 
-    feature_maps = {}
+    backbone = nn.Sequential(*list(yolo_nn.model[:9]))
 
-    def save_features(module, inputs, output):
-        feature_maps["x"] = output
-
-    hook_layer = yolo_nn.model[9]
-    hook_layer.register_forward_hook(save_features)
-
-    backbone = YOLOFeatureBackbone(yolo_nn, feature_maps=feature_maps)
     model = FireSmokeModel(
         backbone, in_channels=config.nb_in_channels, num_classes=config.num_classes
     ).to(config.device)
